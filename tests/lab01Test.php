fbe0a7dc61869f26a9ff2dbf64d37fcd14f6c146
Wouter Groeneveld <wouter.groeneveld@gmail.com>
2016-09-09 14:11:11 +0200

    revealjs slide initial checkin, oefening test update

--- tests/lab01Test.php
+++ tests/lab01Test.php
@@ -4,20 +4,60 @@ require_once('labs/lab01_methods.php');
 
 class Lab01Test extends PHPUnit_Framework_TestCase {
 
-	private $methods;
+	private $theEmpire;
 
 	public function setUp() {
-		$this->methods = new Methods();
+		$this->theEmpire = new Empire();
 	}
 
 	public function tearDown() {
-		$this->methods = null;
+		$this->theEmpire = null;
 	}
 
-	public function testBlabla() {
-		$this->assertEquals(1, $this->methods->blabla());
+	public function testReportWithZeroArmySizeDoesNothing() {
+		$this->theEmpire->report(null, -1);
+		$this->assertEquals(0, sizeof($this->theEmpire->getA()));
 	}
 
+	public function testReportReducesArmySizeIfBattleIsLost() {
+		$sith1 = $this->theEmpire->generate();
+		$sith2 = $this->theEmpire->generate();
+
+		$this->theEmpire->report($sith1, -1);
+		$this->assertEquals(1, sizeof($this->theEmpire->getA()));
+	}
+
+	public function testGetAReturnsArmyStrenghtOfTheEmpire() {
+		$this->assertEquals(0, sizeof($this->theEmpire->getA()));
+
+		$this->theEmpire->generate();
+		$this->theEmpire->generate();
+		$this->theEmpire->generate();
+
+		$this->assertEquals(3, sizeof($this->theEmpire->getA()));
+	}
+
+	public function testGenerateMakesANewSith() {
+		$sith = $this->theEmpire->generate();
+
+		$this->assertInstanceOf('Sith', $sith);
+	}
+
+	public function testExecuteSithAgainstSithKillEachOther() {
+		$sith1 = $this->theEmpire->generate();
+		$sith2 = $this->theEmpire->generate();
+
+		$result = $sith1->execute($sith2);
+		$this->assertEquals(-1, $result);
+	}
+
+	public function testExecuteSithAgainstJediWins() {
+		$sith = $this->theEmpire->generate();
+		$jedi = $this->theEmpire->generate01();
+
+		$result = $sith->execute($jedi);
+		$this->assertEquals(1, $result);
+	}
 }
 
 ?>
